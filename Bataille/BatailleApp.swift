586325d1f1fd2c86e19bbfc3523d159b1d1ba3ac
Steven Vallarsa <orionsjv@gmail.com>
2023-07-08 16:05:22 -0400

    Initial Commit

---
+++ Bataille/BatailleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatailleApp.swift
+//  Bataille
+//
+//  Created by Steven Vallarsa on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BatailleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
